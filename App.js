357a5585c1ebd728973f68bbc6fdd6cfa8e198c3
Viola <violetta.artiukhova@skey.team>
2024-09-11 18:36:06 +0200

    working with core components

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text style={styles.dummyText}>Another piece of text!</Text>
+      </View>
+      <Text style={styles.dummyText}> Hello!</Text>
+      <Button title="Tap me!" />
     </View>
   );
 }
@@ -16,5 +19,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  dummyText: {
+    margin: 16,
+    borderWidth: 2,
+    borderColor: 'red',
+    padding: 16,
   },
 });
